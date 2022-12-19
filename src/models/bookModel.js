9d842952467d3bc60b1487dce86205309a92a480
divyaGrvgithub <divyamalatiwarimtech@gmail.com>
2022-12-19 11:19:10 +0530

    assignment completed

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,23 +1,36 @@
-const mongoose = require('mongoose');
-
-const bookSchema = new mongoose.Schema( {
-    bookName: String, 
-    authorName: String, 
-    tags: [String],
-    
-    isPublished: Boolean,
-    prices: {
-        indianPrice: String,
-        europePrice: String,
-    },
-    stockAvailable : Boolean,
-    totalPages : String,
-    year : Number,
-    sales: {type: Number, default: 10}
-}, { timestamps: true });
-
-
-module.exports = mongoose.model('Book', bookSchema) //users
+const mongoose=require('mongoose')
+ 
+const bookSchema= new mongoose.Schema({
+   bookName: {
+       type: String,
+       required: true
+   },
+   ISBN: {
+       type: String,
+       required: true,
+       unique: true
+   },
+   author: String,
+   tags: [ String ], //array of strings
+   year: Number,
+   isPublished: {
+       type: Boolean, //Boolean
+       default: false
+   },
+   prices: {
+       indianPrice: String,
+       europeanPrice: String,
+       japanPrice: String
+   },
+   sales: {
+       type: Number,
+       default : 0
+   },
+   completionDate: Date
+ 
+}, {timestamps: true} )
+ 
+module.exports = mongoose.model( 'Book', bookSchema ) //users
 
 //Validation:
 //require:true
