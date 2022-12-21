7d835304773aceb656a84892bc5213d4a113b377
divyaGrvgithub <divyamalatiwarimtech@gmail.com>
2022-12-21 20:24:02 +0530

    Assignment completed

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const router = express.Router();
-// const UserModel= require("../models/userModel.js")
-// const UserController= require("../controllers/userController")
-// const BookController= require("../controllers/bookController")
+const UserModel= require("../models/userModel.js")
+const UserController= require("../controllers/userController")
+const BookController= require("../controllers/bookController")
 const AuthorController= require("../controllers/authorController")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-// router.post("/createUser", UserController.createUser  )
-// router.get("/getUsersData", UserController.getUsersData)
-// router.post("/createBook", BookController.createBook  )
-// router.get("/getBooksData", BookController.getBooksData)
-// router.post("/updateBooks", BookController.updateBooks)
-// router.post("/deleteBooks", BookController.deleteBooks)
+router.post("/createUser", UserController.createUser  )
+router.get("/getUsersData", UserController.getUsersData)
+router.post("/createBook", BookController.createBook  )
+router.get("/getBooksData", BookController.getBooksData)
+router.post("/updateBooks", BookController.updateBooks)
+router.post("/deleteBooks", BookController.deleteBooks)
 router.post("/createAuthor", AuthorController.createAuthor)
 router.post("/createBooks", AuthorController.createBooks)
 router.get("/getBookByAuthor", AuthorController.getBookByAuthor)
