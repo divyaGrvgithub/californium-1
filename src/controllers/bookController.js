eeda096b529f3618635f62bc71bd390c742fd7cb
divyaGrvgithub <divyamalatiwarimtech@gmail.com>
2022-12-22 23:28:01 +0530

    assignment completed

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -9,9 +9,11 @@ const createBook= async function (req, res) {
     let authorId = book.author
     let publisherId = book.publishername
         if (authorId) {
-        if(!publisherId) res.send({msg : "publisher  id is needed" })
+        if(!publisherId) 
+        res.send({msg : "publisher  id is needed" })
     }
-    else  res.send({msg : " author  id is needed" })
+    else  
+    res.send({msg : " author  id is needed" })
     
    let bookCreated = await bookModel.create(book)
    res.send({msg : bookCreated})
@@ -21,7 +23,7 @@ const getBooksData= async function (req, res) {
     let books = await bookModel.find()
     res.send({data: books})
 }
-//--------------------------3
+//--------------------------3-----------------------------
 const bookAuthor= async function (req, res) {
        let authorbookpublisher = await bookModel.find().populate('author').populate('publishername')
     res.send({data: authorbookpublisher})
@@ -39,7 +41,7 @@ const attribute= async function(req, res){
     }
     res.send("Check the console")
 }
-///-----------------------------------last--------
+///-----------------------------------last-------------------------
 const update=async function(req, res){
 
     let key = await authorModel.find({ rating:{$gt:3.5}}).select({_id:1});
