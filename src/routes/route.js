eaeff84899284c6d2778018dd7381fd78b85416c
divyaGrvgithub <divyamalatiwarimtech@gmail.com>
2022-12-28 17:58:52 +0530

    try catch assignment completed

--- src/routes/route.js
+++ src/routes/route.js
@@ -9,16 +9,15 @@ router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
+router.post("/createBook", BookController.createBook)
+router.get("/getBooksData",BookController.getBooksData)
+router.put("/updateBooks",BookController.updateBooks)
+router.delete("/deleteBooks",BookController.deleteBooks)
+router.get("/totalSalesPerAuthor",BookController.totalSalesPerAuthor)
 
-
-
-router.post("/createBook", BookController.createBook  )
-
-
-
-
-// router.post("/createUser", UserController.createUser  )
-// router.get("/getUsersData", UserController.getUsersData)
+router.post("/createUser", UserController.createUser)
+router.get("/getUsersData", UserController.getUsersData)
+router.get("/basicRoute", commonMW.mid1, commonMW.mid2, commonMW.mid3, commonMW.mid4, UserController.basicCode)
 
 
 // const mid1= function ( req, res, next) {
@@ -43,20 +42,9 @@ router.post("/createBook", BookController.createBook  )
 
 // router.get('/termsAndConditions',  UserController.termsAndConditions)
 // router.get('/register',  UserController.register)
-
-
-
-
-
-router.get("/basicRoute", commonMW.mid1, commonMW.mid2, commonMW.mid3, commonMW.mid4, UserController.basicCode)
-
-
-
+// router.get("/basicRoute", commonMW.mid1, commonMW.mid2, commonMW.mid3, commonMW.mid4, UserController.basicCode)
 // router.get("/basicRoute2", commonMW.mid1, UserController.basicCode2)
 // router.get("/basicRoute3", commonMW.mid2, UserController.basicCode3)
 // router.get("/basicRoute4", commonMW.mid1, commonMW.mid4, UserController.basicCode4)
-
-
-
 
 module.exports = router;
