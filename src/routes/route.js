a9e66a4d22c44bdb67baa142a2213d4eefa6dcd6
divyaGrvgithub <divyamalatiwarimtech@gmail.com>
2022-12-29 09:17:46 +0530

    authorisation assignment completed

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController= require("../controllers/userController")
+const middleware = require("../middleware/auth")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
@@ -9,9 +10,9 @@ router.get("/test-me", function (req, res) {
 router.post("/users", userController.createUser)
 router.post("/login", userController.loginUser)
 //The userId is sent by front end
-router.get("/users/:userId", userController.getUserData)
-router.post("/users/:userId/posts", userController.postMessage)
-router.put("/users/:userId", userController.updateUser)
-router.delete('/users/:userId', userController.deleteUser)
+router.get("/users/:userId",middleware.authenticate,userController.getUserData)
+router.post("/users/:userId/posts", middleware.authenticate,userController.postMessage)
+router.put("/users/:userId",middleware.authenticate,userController.updateUser)
+router.delete('/users/:userId',middleware.authenticate,userController.deleteUser)
 
 module.exports = router;
